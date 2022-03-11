e4b2284ede4f22f6e43c69c320cc066b3bf06b97
kevin FERRANDON <kferrandon@gmail.com>
2022-03-11 15:50:28 +0100

    first add

---
+++ formulaire.php
@@ -0,0 +1,11 @@
+<form>
+ <!--   <label for="nom">Nom </label>
+           <input type="text" placeholder="dupond" name="nom" id="nom" />
+ -->
+    <label for="mail">Mail</label>
+    <input type="email" placeholder="dupond@domaine.fr" name="mail" id="mail" />
+    <label for="mdp">Mot de passe</label>
+    <input type="password" name="password" id="password" />
+   
+    <button type="submit"  >Se Connecter</button>
+</form>
