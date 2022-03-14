f9d7151bbc5846f6847af41b090c8d5f1cf7e687
kevin FERRANDON <kevin.ferrandon@lyceefulbert.fr>
2022-03-14 08:52:03 +0100

    add cookies

--- index.php
+++ index.php
@@ -196,6 +196,6 @@ $taille = isset($_GET['taille']) ? $_GET['taille'] : 600;
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+        <script src="js/scripts.js"></script>>
     </body>
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-/*!
-* Start Bootstrap - Shop Item v5.0.4 (https://startbootstrap.com/template/shop-item)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
-*/
+
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+document.cookie = "username=dior; expires=Thu, 18 Dec 2022 12:00:00 UTC; path=/";
+document.cookie = "token=F4F263E439CF40925E6A412387A9472A6773C2580212A4FB50D224D3A817DE17; expires=Thu, 18 Dec 2022 12:00:00 UTC; path=/";
